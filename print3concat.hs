bd219e57424f770ec6ccab53fb2d1bdb3b2cf1c2
Your Name <you@example.com>
2024-05-15 01:14:02 +0300

    rvrs

--- print3concat.hs
+++ print3concat.hs
@@ -1,23 +1,26 @@
-module Print3 where 
+module Print3 where
+
 -- this was to test if different module name than the file breaks the compiling
-
-
 
 hello :: String
 hello = "hello"
 
-
 world :: String
 world = "world"
 
+thirdLetter :: String -> Char
+thirdLetter x = x !! 2
 
-main :: IO()
-main = do 
-    putStrLn myGreeting
-    putStrLn anotherGreeting
-    where anotherGreeting =
-            concat [hello," ", world] 
-            
+letterIndex :: Int -> [Char] -> Char
+letterIndex x s = s !! x
+
+main :: IO ()
+main = do
+  putStrLn myGreeting
+  putStrLn anotherGreeting
+  where
+    anotherGreeting =
+      concat [hello, " ", world]
 
 --  This is to test if the order matters of declarations, but fortunately not
 myGreeting :: String
---
+++ reverse.hs
@@ -0,0 +1,7 @@
+module Reverse where
+
+rvrs :: String -> String
+rvrs s = s -- to do
+
+main :: IO ()
+main = print $ rvrs "carry is karen"
